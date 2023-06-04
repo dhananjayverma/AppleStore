5b1b3e3ebbcf46770e3d414ce82ff3ccffdd717a
Dhananjay kumar verma <dhananjayverma088@gmail.com>
2023-06-04 18:42:48 +0530

    react-redux ecom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import Header from './components/Header';
+// import Footer from './components/Footer'
+// import Home from './components/Home'
+// import About from './components/About'
+// import Product from './components/Product'
+// import Contact from './components/Contact'
+// import { Redirect, Route, Switch } from 'react-router-dom';
+// import ProductDetail from './components/ProductDetail';
+// import Cart from './components/Cart';
+// import Checkout from './components/Checkout'
+
+// function App() {
+//   return (
+//     <>
+//       <Header/>
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route exact path="/products" component={Product} />
+//         <Route exact path="/products/:id" component={ProductDetail} />
+//         <Route exact path="/cart" component={Cart} />
+//         <Route exact path="/checkout" component={Checkout} />
+//         <Route exact path="/about" component={About} />
+//         <Route exact path="/contact" component={Contact} />
+//         <Redirect to="/" />
+//       </Switch>
+//       <Footer/>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Product from './components/Product';
+import Contact from './components/Contact';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Products</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Update import statement
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render( // Update createRoot function
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: If you have any web vitals reporting, you can keep the existing code
 reportWebVitals();
+
+
